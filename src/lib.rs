5999b671afa642296c1362272c3c9db2bd574387
Jonas Seiler <Me@jonasseiler.de>
2024-03-17 21:36:35 +0100

    add functions for getting the Wort-des-Tages

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use select::{
     document::Document,
-    predicate::{Attr, Name, Predicate},
+    predicate::{Attr, Class, Name, Predicate},
 };
 
 pub struct Wort {
@@ -22,11 +22,76 @@ While the library is good, the documentation is lacking a bit, therefore here ar
 
 */
 
+pub async fn get_wort_des_tages() -> Result<Wort, String> {
+    let wort = get_wort_des_tages_link_name().await?;
+
+    get_wort(&wort).await
+}
+
 pub async fn get_wort(wort: &str) -> Result<Wort, String> {
     let doc = fetch_document_for_wort(wort).await?;
 
     Err("lol".to_string())
 }
+
+async fn get_wort_des_tages_link_name() -> Result<String, String> {
+    let url = "https://www.duden.de/wort-des-tages";
+
+    let fetched_html_response = match reqwest::get(url).await {
+        Ok(response) => response,
+        Err(e) => {
+            return Err(format!(
+                "Reqwest error while fetching Wort-des-Tages site: {}",
+                e
+            ))
+        }
+    };
+
+    let fetched_html = match fetched_html_response.text().await {
+        Ok(text) => text,
+        Err(e) => {
+            return Err(format!(
+                "Reqwest error while fetching text from Wort-des-Tages site: {}",
+                e
+            ))
+        }
+    };
+
+    let document = Document::from(fetched_html.as_str());
+
+    // the link for the description page of the Wort-des-Tages is contained in this element
+    let link_predicate = Name("a").and(Class("scene__title-link"));
+
+    // find the node first
+    let link_node = match document.find(link_predicate).next() {
+        Some(node) => node,
+        None => return Err(
+            "Scraping error while searching for Wort-des-Tages Wort-Link: \"a\" element not found"
+                .to_string(),
+        ),
+    };
+
+    // then extract the link
+    let link = match link_node.attr("href") {
+        Some(value) => value,
+        None => return Err(
+            "Scraping error while searching for Wort-des-Tages Wort-Link: \"a\" element does not have \"href\" attribute"
+                .to_string(),
+        ),
+    };
+
+    // and finally strip the prefix to be left with the word as it will be written in the url
+    let link_name = match link.strip_prefix("/rechtschreibung/") {
+        Some(s) => s,
+        None => return Err(
+            "Scraping error while searching for Wort-des-Tages Wort-Link: link does not have correct scheme \"/rechtschreibung/{wort}\""
+                .to_string(),
+        ),
+    };
+
+    Ok(link_name.to_string())
+}
+
 async fn fetch_document_for_wort(wort: &str) -> Result<Document, String> {
     let url = format!("https://www.duden.de/rechtschreibung/{}", wort);
 
@@ -47,6 +112,7 @@ async fn fetch_document_for_wort(wort: &str) -> Result<Document, String> {
 
     Ok(Document::from(fetched_html.as_str()))
 }
+
 fn get_wort_from_document(doc: &Document) -> Result<String, String> {
     let meta_tag_predicate = Name("meta").and(Attr("property", "og:title"));
 
