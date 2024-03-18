23a9a39fafec08a4c2ee1af5f7e830a1439de831
Jonas Seiler <Me@jonasseiler.de>
2024-03-18 15:57:29 +0100

    add Wort-query binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use duden_rs_lib::get_wort_des_tages;
+use std::io;
+
+use duden_rs_lib::get_wort;
+
+#[tokio::main]
+async fn main() {
+    println!("Wort des Tages:");
+
+    match get_wort_des_tages().await {
+        Ok(s) => println!("{}", s),
+        Err(e) => eprintln!("{}", e),
+    };
+
+    loop {
+        println!("Please insert word as it would appear in the url:");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match get_wort(&input).await {
+            Ok(s) => println!("{}", s),
+            Err(e) => eprintln!("{}", e),
+        };
+    }
 }
